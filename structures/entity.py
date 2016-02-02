2f8901162fcac99d3517ee6abe16415ed5da51d8
stasszczesniak <stas.szczesniak@gmail.com>
2016-02-02 14:50:17 +0100

    Added entities classes.

--- structures/entity.py
+++ structures/entity.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 
-# Copyright (C) 2014  Ben Ockmore, Stanisław Szcześniak
+# Copyright (C) 2014-2016 Ben Ockmore, Stanisław Szcześniak
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,16 +18,14 @@
 """This module specifies a class, Resource, which is designed to be used as the
 base class for all resource models specified in this package."""
 
+from structures.base import Base
 
-class Base:
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
 
 class Entity(Base):
     """Resource class, from which all other resource models are derived."""
     entity_gid = None
     uri = None
+    type = None
 
     default_alias_id = None
 
@@ -49,3 +47,97 @@ class Entity(Base):
     relationships_uri = None
 
     revision = None
+
+
+class Creator(Entity):
+    creator_type = None
+
+    begin_date = None
+    begin_date_precision = None
+
+    @property
+    def begin(self):
+        return format_date(self.begin_date, self.begin_date_precision)
+
+    end_date = None
+    end_date_precision = None
+
+    @property
+    def end(self):
+        return format_date(self.end_date, self.end_date_precision)
+
+    ended = None
+
+    country_id = None
+    gender_id = None
+
+    gender = None
+    creator_type = None
+
+
+class Publication(Entity):
+    publication_type = None
+
+class Edition(Entity):
+    edition_type = None
+
+    release_date = None
+    release_date_precision = None
+
+    @property
+    def release(self):
+        return format_date(self.release_date, self.release_date_precision)
+
+    pages = None
+
+    # Dimensions, stored as integer millimetre values
+    width = None
+    height = None
+    depth = None
+
+    # Weight in grams
+    weight = None
+
+    creator_credit = None
+    language = None
+    edition_format = None
+    edition_status = None
+    publisher = None
+    publication = None
+
+
+class Publisher(Entity):
+    publisher_type = None
+
+    begin_date = None
+    begin_date_precision = None
+
+    @property
+    def begin(self):
+        return format_date(self.begin_date, self.begin_date_precision)
+
+    end_date = None
+    end_date_precision = None
+
+    @property
+    def end(self):
+        return format_date(self.end_date, self.end_date_precision)
+
+    ended = None
+
+
+class Work(Entity):
+    work_type = None
+    languages = None
+
+
+def format_date(date, precision):
+    if date is None:
+        return None
+
+    if precision == 'YEAR':
+        return '{:02}'.format(date.year)
+    elif precision == 'MONTH':
+        return '{:02}-{:02}'.format(date.year, date.month)
+    else:
+        return '{:02}-{:02}-{:02}'.format(date.year, date.month, date.day)
