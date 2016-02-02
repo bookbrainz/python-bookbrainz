dae829416da96aeca7c9e4a1e0c6f316d75e47d8
stasszczesniak <stas.szczesniak@gmail.com>
2016-02-02 15:13:16 +0100

    Updated __init__.py

--- structures/__init__.py
+++ structures/__init__.py
@@ -1,1 +1,5 @@
-import base
+from base import Base
+from entity import Entity, Creator, Work, Publisher, Publication, Edition
+from entity_types import CreatorType, WorkType, PublisherType, PublicationType
+from simple_objects import Identifier, IdentifierType, Alias, Annotation,\
+    Disambiguation, Language, EditionStatus, EditionFormat, Gender
