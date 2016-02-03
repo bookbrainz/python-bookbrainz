97109ddf7259f2b226772c5e598caefcf1e678ce
stasszczesniak <stas.szczesniak@gmail.com>
2016-02-03 22:38:24 +0100

    Fixed __init__.py and added there recently created classes

--- pybb/__init__.py
+++ pybb/__init__.py
@@ -1,5 +1,15 @@
 from base import Base
-from entity import Entity, Creator, Work, Publisher, Publication, Edition
+
+from entity import Entity
+from creator import Creator
+from work import Work
+from edition import Edition
+from publisher import Publisher
+from publication import Publication
 from entity_types import CreatorType, WorkType, PublisherType, PublicationType
+
+from relationship import Relationship
+from revision import EntityRevision
+
 from simple_objects import Identifier, IdentifierType, Alias, Annotation,\
     Disambiguation, Language, EditionStatus, EditionFormat, Gender
