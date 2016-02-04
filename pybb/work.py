2e21ce3b874ee4c1af7881473bb030f1fd5e56bc
stasszczesniak <stas.szczesniak@gmail.com>
2016-02-04 21:37:37 +0100

    Class name was wrong

--- pybb/work.py
+++ pybb/work.py
@@ -20,7 +20,7 @@ from entity_types import WorkType
 from simple_objects import Language
 
 
-class Publisher(Entity):
+class Work(Entity):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.work_type = None
