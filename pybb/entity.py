099df5068b22e61804ac2914ab709e557b40b02f
stasszczesniak <stas.szczesniak@gmail.com>
2016-02-06 19:26:20 +0100

    Should be Entity not self.__class__

--- pybb/entity.py
+++ pybb/entity.py
@@ -29,7 +29,7 @@ from pybb import default_agent
 
 class Entity(Base):
     def __init__(self):
-        super(self.__class__, self).__init__()
+        super(Entity, self).__init__()
         self.entity_gid = None
         self.uri = None
         self.type = None
