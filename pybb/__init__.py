90d9d3607c89b2749dd55264cc81a1ea0eba35f3
stasszczesniak <stas.szczesniak@gmail.com>
2016-02-12 18:58:54 +0100

    Relationship-related classes were missing

--- pybb/__init__.py
+++ pybb/__init__.py
@@ -6,7 +6,8 @@ from entity import Entity
 from entity_types import CreatorType, WorkType, PublisherType, PublicationType
 from publication import Publication
 from publisher import Publisher
-from relationship import Relationship
+from relationship import Relationship, RelationshipType, \
+    RelationshipEntity, RelationshipText
 from revision import EntityRevision
 from simple_objects import Identifier, IdentifierType, Alias, Annotation,\
     Disambiguation, Language, EditionStatus, EditionFormat, Gender
